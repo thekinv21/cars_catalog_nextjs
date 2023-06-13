31c1f5df33659b0ed0313aac4570e613d374ed36
thekinv21 <vadimkiniabaev@gmail.com>
2023-06-13 03:19:43 +0300

    create all imports file

---
+++ import/index.ts
@@ -0,0 +1,19 @@
+import Main from '@/screens/main/Main'
+
+import CustomFooter from '@/components/layout/footer/CustomFooter'
+import CustomNavbar from '@/components/layout/navbar/CustomNavbar'
+
+import CustomButton from '@/ui/button/CustomButton'
+import CustomFilter from '@/ui/filter/CustomFilter'
+import CustomSearchMenu from '@/ui/menu/CustomSearchMenu'
+import CustomSearchBar from '@/ui/searchbar/CustomSearchBar'
+
+export {
+	CustomButton,
+	CustomFilter,
+	CustomFooter,
+	CustomNavbar,
+	CustomSearchBar,
+	CustomSearchMenu,
+	Main
+}

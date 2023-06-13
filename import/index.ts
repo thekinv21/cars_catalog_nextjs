2ae8a40ad88aa53e3b7bbbf505056ef39070b314
thekinv21 <vadimkiniabaev@gmail.com>
2023-06-14 01:28:43 +0300

    refactoring components

--- import/index.ts
+++ import/index.ts
@@ -1,19 +1,9 @@
+import CustomSearchBar from '@/screens/main/car-catalog/searchbar/CustomSearchBar'
 import Main from '@/screens/main/Main'
 
 import CustomFooter from '@/components/layout/footer/CustomFooter'
 import CustomNavbar from '@/components/layout/navbar/CustomNavbar'
 
 import CustomButton from '@/ui/button/CustomButton'
-import CustomFilter from '@/ui/filter/CustomFilter'
-import CustomSearchMenu from '@/ui/menu/CustomSearchMenu'
-import CustomSearchBar from '@/ui/searchbar/CustomSearchBar'
 
-export {
-	CustomButton,
-	CustomFilter,
-	CustomFooter,
-	CustomNavbar,
-	CustomSearchBar,
-	CustomSearchMenu,
-	Main
-}
+export { CustomButton, CustomFooter, CustomNavbar, CustomSearchBar, Main }

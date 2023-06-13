1f42e227fe767dd0897cec8a740fcd7106b6304e
thekinv21 <vadimkiniabaev@gmail.com>
2023-06-14 01:29:58 +0300

    create api take cars with name

--- api/api.ts
+++ api/api.ts
@@ -3,6 +3,21 @@ import { END_POINTS } from '@/constants/endpoints'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 
+//*============GET CARS BY NAME===========
+const getCarWithName = async () => {
+	const URL = END_POINTS.CAR_CONTROLLER.GET_WITH_NAME
+
+	const repsonse = await axios.get(URL, { headers: apiKey.headers })
+	return { repsonse }
+}
+
+export const useGetByName = () => {
+	const { data, isLoading, refetch } = useQuery(['getCarsWithName'], () =>
+		getCarWithName()
+	)
+	return { data, isLoading, refetch }
+}
+
 //*============GET CARS BY MODEL===========
 const getCarWithModel = async () => {
 	const URL = END_POINTS.CAR_CONTROLLER.GET_WITH_MODEL
